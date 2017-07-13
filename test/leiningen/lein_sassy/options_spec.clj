85308b753dbdc35b7b9e714068c4ef0d3d2a084e
Alejandro Peinó <alepeino@gmail.com>
2017-07-12 23:56:04 -0300

    Options test moved to own ns

---
+++ test/leiningen/lein_sassy/options_spec.clj
@@ -0,0 +1,30 @@
+(ns leiningen.lein-sassy.options-spec
+  (:require [clojure.test :refer :all]
+            [leiningen.lein-sassy.options :refer :all]
+            [leiningen.lein-sassy.renderer :refer :all]))
+
+(def default-options {:src "resources/public/stylesheets"
+                      :dst "resources/app/stylesheets"
+                      :gem-name "sass"
+                      :gem-version "3.2.14"
+                      :delete-output-dir true
+                      :style :nested})
+
+(def project {:sass {:src "test/files-in/sass"
+                     :dst "test/files-out/"
+                     :syntax :sass
+                     :style :expanded}})
+
+(deftest get-sass-options-test
+
+  (testing "merges project with defaults, adding default load paths"
+    (is (= (merge
+             default-options
+             {:load_paths [(get-in project [:sass :src])]}
+             (:sass project))
+           (get-sass-options project))))
+
+  (testing "throws warning when no sass entry in project"
+    (binding [*err* (java.io.StringWriter.)]
+      (get-sass-options {:foo "bar"})
+      (is (= "No sass entry found in project definition.\n" (str *err*))))))
--- test/leiningen/lein_sassy/renderer_spec.clj
+++ test/leiningen/lein_sassy/renderer_spec.clj
@@ -1,31 +1,14 @@
 (ns leiningen.lein-sassy.renderer-spec
-  (:require [clojure.test :refer :all]
-            [leiningen.lein-sassy.options :refer :all]
-            [leiningen.lein-sassy.renderer :refer :all]))
-
-(def default-options {:src "resources/public/stylesheets"
-                      :dst "resources/app/stylesheets"
-                      :gem-name "sass"
-                      :gem-version "3.2.14"
-                      :delete-output-dir true
-                      :style :nested})
+  (:require
+    [clojure.java.io :as io]
+    [clojure.test :refer :all]
+    [leiningen.lein-sassy.options :refer :all]
+    [leiningen.lein-sassy.renderer :refer :all]))
 
 (def project {:sass {:src "test/files-in/sass"
                      :dst "test/files-out/"
                      :syntax :sass
                      :style :expanded}})
-
-(deftest get-sass-options-test
-  (testing "merges project with defaults, adding default load paths"
-    (is (= (merge
-             default-options
-             {:load_paths [(get-in project [:sass :src])]}
-             (:sass project))
-           (get-sass-options project))))
-  (testing "throws warning when no sass entry in project"
-    (binding [*err* (java.io.StringWriter.)]
-      (get-sass-options {:foo "bar"})
-      (is (= "No sass entry found in project definition.\n" (str *err*))))))
 
 (deftest render-file-test
 
